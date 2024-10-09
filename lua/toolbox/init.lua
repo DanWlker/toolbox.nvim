f2e132a7b3a483c310061efe328ceb1622232f34
DanWlker <danielhee2@gmail.com>
2024-10-09 20:34:47 +0800

    enhanced to handle functions that contain arguments, and commands that may need to be edited

--- lua/toolbox/init.lua
+++ lua/toolbox/init.lua
@@ -6,12 +6,26 @@ function M.setup(opts)
 
 	M.commandMap = {}
 	for _, val in ipairs(commands) do
-		M.commandMap[val.name] = val.execute
+		M.commandMap[val.name] = {
+			execute = val.execute,
+			require_input = val.require_input or false,
+		}
 	end
 
 	M.commandKeyList = {}
 	for k, _ in pairs(M.commandMap) do
 		table.insert(M.commandKeyList, k)
+	end
+end
+
+function M.run(name, ...)
+	local execute = M.commandMap[name].execute
+	if execute == nil or type(execute) ~= "function" then
+		error("Unknown or unexecutable command", 0)
+	end
+	local ok, res = pcall(execute, ...)
+	if not ok then
+		error(res, 0)
 	end
 end
 
@@ -23,17 +37,36 @@ function M.show_picker()
 			return
 		end
 
-		local execute = M.commandMap[choice]
+		local execute = M.commandMap[choice].execute
 		if execute == nil then
 			return
 		end
+
 		if type(execute) == "function" then
+			if debug.getinfo(execute).nparams > 0 then
+				vim.api.nvim_feedkeys(
+					vim.api.nvim_replace_termcodes(
+						":lua require('toolbox').run(\"" .. choice .. '", )<Left>',
+						true,
+						false,
+						true
+					),
+					"m",
+					true
+				)
+				return
+			end
 			local ok, res = pcall(execute)
 			if not ok then
 				error(res, 0)
 			end
 		end
+
 		if type(execute) == "string" then
+			if M.commandMap[choice].require_input then
+				vim.api.nvim_feedkeys(":" .. execute, "m", true)
+				return
+			end
 			local ok, res = pcall(vim.cmd, execute)
 			if not ok then
 				error(res, 0)
